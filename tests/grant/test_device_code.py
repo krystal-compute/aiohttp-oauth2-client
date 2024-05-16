8ba9251ad149a2d102cb660836f0244ea521df59
Stijn Caerts <stijn.caerts@vito.be>
2024-05-16 10:12:37 +0200

    test: move response mock functions

--- tests/grant/test_device_code.py
+++ tests/grant/test_device_code.py
@@ -1,41 +1,35 @@
 from aioresponses import aioresponses
 from yarl import URL
 
+from aiohttp_oauth2_client.client import OAuth2Client
 from aiohttp_oauth2_client.grant.device_code import DeviceCodeGrant
 from aiohttp_oauth2_client.models.response import (
     DeviceAuthorizationResponse,
     ErrorResponse,
 )
+from ..conftest import assert_request_with_access_token
 from ..constants import TOKEN_ENDPOINT, DEVICE_AUTHORIZATION_ENDPOINT
-from ..mock.response import add_token_request
+from ..mock.response import (
+    add_token_request,
+    add_token_request_error,
+    add_device_authorization_request,
+)
 
 CLIENT_ID = "test-client"
 DEVICE_CODE = "device-code"
 
-
-def add_device_authorization_request(mock: aioresponses):
-    device_authorization_response = DeviceAuthorizationResponse(
-        device_code=DEVICE_CODE,
-        user_code="user-code",
-        verification_uri="verification-uri",
-        expires_in=300,
-        interval=5,
-    )
-    mock.post(
-        DEVICE_AUTHORIZATION_ENDPOINT,
-        status=200,
-        payload=device_authorization_response.model_dump(exclude_none=True),
-    )
-
-
-def add_token_request_authorization_pending(mock: aioresponses):
-    error = ErrorResponse(error="authorization_pending")
-    mock.post(TOKEN_ENDPOINT, status=400, payload=error.model_dump(exclude_none=True))
+device_authorization_response = DeviceAuthorizationResponse(
+    device_code=DEVICE_CODE,
+    user_code="user-code",
+    verification_uri="verification-uri",
+    expires_in=300,
+    interval=5,
+)
 
 
 async def test_fetch_token(mock_token: dict, mock_responses: aioresponses):
     # set up mock responses
-    add_device_authorization_request(mock_responses)
+    add_device_authorization_request(mock_responses, device_authorization_response)
     add_token_request(mock_responses, mock_token)
 
     async with DeviceCodeGrant(
@@ -66,9 +60,11 @@ async def test_fetch_token(mock_token: dict, mock_responses: aioresponses):
 
 
 async def test_wait_for_token(mock_token: dict, mock_responses: aioresponses):
-    add_device_authorization_request(mock_responses)
+    add_device_authorization_request(mock_responses, device_authorization_response)
     # first token request results in "authorization_pending" error
-    add_token_request_authorization_pending(mock_responses)
+    add_token_request_error(
+        mock_responses, error=ErrorResponse(error="authorization_pending")
+    )
     # second token requests is successful
     add_token_request(mock_responses, mock_token)
 
@@ -88,7 +84,7 @@ async def test_wait_for_token(mock_token: dict, mock_responses: aioresponses):
 async def test_refresh_token(
     mock_token: dict, mock_token2: dict, mock_responses: aioresponses
 ):
-    add_device_authorization_request(mock_responses)
+    add_device_authorization_request(mock_responses, device_authorization_response)
     add_token_request(mock_responses, mock_token)
     add_token_request(mock_responses, mock_token2)
 
@@ -112,3 +108,31 @@ async def test_refresh_token(
                 "refresh_token": mock_token["refresh_token"],
             },
         )
+
+
+async def test_client(mock_token: dict, mock_responses: aioresponses):
+    add_device_authorization_request(mock_responses, device_authorization_response)
+    add_token_request(mock_responses, mock_token)
+    async with DeviceCodeGrant(
+        token_url=TOKEN_ENDPOINT,
+        device_authorization_url=DEVICE_AUTHORIZATION_ENDPOINT,
+        client_id=CLIENT_ID,
+    ) as grant, OAuth2Client(grant) as client:
+        await assert_request_with_access_token(client, mock_token, mock_responses)
+
+
+async def test_client_refresh(
+    mock_token: dict, mock_token2: dict, mock_responses: aioresponses
+):
+    add_device_authorization_request(mock_responses, device_authorization_response)
+    add_token_request(mock_responses, mock_token)
+    add_token_request(mock_responses, mock_token2)
+    async with DeviceCodeGrant(
+        token_url=TOKEN_ENDPOINT,
+        device_authorization_url=DEVICE_AUTHORIZATION_ENDPOINT,
+        client_id=CLIENT_ID,
+    ) as grant, OAuth2Client(grant) as client:
+        await assert_request_with_access_token(client, mock_token, mock_responses)
+        grant.token.expires_at = 1
+        assert grant.token.is_expired()
+        await assert_request_with_access_token(client, mock_token2, mock_responses)
--- tests/mock/response.py
+++ tests/mock/response.py
@@ -1,7 +1,26 @@
 from aioresponses import aioresponses
 
-from ..constants import TOKEN_ENDPOINT
+from aiohttp_oauth2_client.models.response import (
+    ErrorResponse,
+    DeviceAuthorizationResponse,
+)
+
+from ..constants import TOKEN_ENDPOINT, DEVICE_AUTHORIZATION_ENDPOINT
 
 
 def add_token_request(mock: aioresponses, token: dict):
     mock.post(TOKEN_ENDPOINT, status=200, payload=token)
+
+
+def add_token_request_error(mock: aioresponses, error: ErrorResponse):
+    mock.post(TOKEN_ENDPOINT, status=400, payload=error.model_dump(exclude_none=True))
+
+
+def add_device_authorization_request(
+    mock: aioresponses, response: DeviceAuthorizationResponse
+):
+    mock.post(
+        DEVICE_AUTHORIZATION_ENDPOINT,
+        status=200,
+        payload=response.model_dump(exclude_none=True),
+    )
